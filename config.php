b21d65a1da40ae7d90fbe3825a921e3edd96f408
Berenike Re <32675484+BerenikeRe@users.noreply.github.com>
2023-03-05 23:41:53 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,7 @@ return [
     'relationId' => 62400,
 
     // Languages in which you want to extract Wiki informations
-    'languages' => ['en'],
-    'languages' => ['de'],
+    'languages' => ['de', 'en'],
 
     // Exclude datas from beeing extracted. Usefull to exclude streets at the edge of the city for example
     'exclude' => [
